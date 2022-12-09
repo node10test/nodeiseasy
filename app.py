19e9c41b71de0e2b25588cfe6e5ecfaf3a04a549
Jo haebeen <haebin1622@naver.com>
2022-12-09 09:49:02 +0900

    주석 추가

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def get_users(id):
 
     db.commit()
     db.close()
-
+    # 결과 값 저장
     result = {
         "name": rows[0][0],
         "desc": rows[0][1],
@@ -77,10 +77,9 @@ def post_users(id):
     flash("수정이 완료되었습니다")
     return render_template("my_page.html")
 
-    # return redirect('/')
-    # return jsonify({'msg': '수정이 완료되었습니다'}), 200
 
-    # 파일 업로드
+# 파일 업로드
+# rsplit은 뒤에서 부터의 값을 끊어주는 것 = 파일의 확장자명 저장
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -88,26 +87,33 @@ def allowed_file(filename):
 def upload():
     # 아래 섹션 나중에 지워야됨
     session['email'] = "haebin1622@naver.com"
-    # print(0)
     db = pymysql.connect(user="root", passwd="qwe1357asd!", host="localhost", db="dailycafe", charset="utf8")
     cur = db.cursor(pymysql.cursors.DictCursor)
-    # print(1)
+    # 저장 시간 저장
     now = datetime.now()
+    # 이메일 세션 저장
     up_email = session['email']
-    # print(up_email)
+    # method = post 이면
     if request.method == 'POST':
+         # input 타입의 태그 이름을 files에 저장
         files = request.files.getlist('files[]')
-        # print(files)
         for file in files:
+            # 만약 파일이 {'png', 'jpg', 'jpeg'} 이와 같은 확장자와 일치하면
             if file and allowed_file(file.filename):
+                # 어려워
                 filename = secure_filename(file.filename)
+                # 이건 더
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                # users 테이블 안에 있는 세션 저장한 email의 img, upload_time의 컬럼 값을 변경
                 sql = f'UPDATE users SET img="{filename}",upload_time="{now}" WHERE email="{up_email}";'
+                # sql 쿼리문 실행
                 cur.execute(sql)
+                # 커밋 및 연결 종료
                 db.commit()
-            # print(file)
-        db.close()
+                db.close()
+        # alert 띄워주기
         flash('사진이 성공적으로 업로드 되었어요!')
+    # edit 페이지로 돌아가기
     return redirect('/edit_page')
 
 if __name__ == '__main__':
